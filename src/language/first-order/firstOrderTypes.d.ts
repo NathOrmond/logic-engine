095b1254cf24db5c109ae22887444fc055394ec2
Nathan Ormond <nath.m.ormond@gmail.com>
2024-02-04 12:53:16 +0000

    test: lofical complex tests

--- src/language/first-order/firstOrderTypes.d.ts
+++ src/language/first-order/firstOrderTypes.d.ts
@@ -1,6 +1,21 @@
 
+/**
+ * Unary Logical operators function on a single 
+ * truth evaluable statement (WFF - Well Formed Formula)
+ * 
+ * ~ := NOT
+ */
 type UnaryOperator = '~';
 
+/**
+ * Binary Logical operators function as connectives 
+ * between two truth evaluable statements (WFFS - Well Formed Formulas)
+ * 
+ * & := AND 
+ * | := OR
+ * -> := Material Implication (if ... then ...)
+ * <-> := Biconditional (if and only if)
+ */
 type BinaryOperator = '&' | '|' | '->' | '<->';
 
 type WFF  = Atom | Complex; 
--- test/language/first-order/complex.spec.ts
+++ test/language/first-order/complex.spec.ts
@@ -1,5 +1,58 @@
+import { AtomImpl } from '../../../src/language/first-order/atom';
+import { ComplexImpl } from '../../../src/language/first-order/complex';
+
 describe('Complex Tests', () => {
-  test.skip('TODO', () => {
-    fail();
+  
+  test('Complexes should apply unary operators to values', () => {
+    let complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '&', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl('~', new AtomImpl(undefined, true), '&', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(false);
   });
+
+  test('Complexes should apply logical AND correctly', () => { 
+    let complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '&', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '&', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(false);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '&', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(false);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '&', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(false);
+  });
+
+  test('Complexes should apply logical OR correctly', () => { 
+    let complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '|', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '|', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '|', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '|', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(false);
+  });
+
+  test('Complexes should apply logical MATERIAL IMPLICATION correctly', () => { 
+    let complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '->', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '->', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(false);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '->', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '->', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(true);
+  });
+
+  test('Complexes should apply logical BICONDITIONAL correctly', () => { 
+    let complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '<->', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(true);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, true), '<->', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(false);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '<->', new AtomImpl(undefined, true));
+    expect(complex.value()).toBe(false);
+    complex = new ComplexImpl(undefined, new AtomImpl(undefined, false), '<->', new AtomImpl(undefined, false));
+    expect(complex.value()).toBe(true);
+  });
+
+
 });
